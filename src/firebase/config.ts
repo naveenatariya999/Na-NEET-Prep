61aa335448a3a780714e42efaa0cd2011f4a3d51
naveen atariya <naveenatariya999@gmail.com>
2026-01-27 12:33:42 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5718849531-9c652",
+  "appId": "1:589442988743:web:cc810401fb138b83935ce2",
+  "apiKey": "AIzaSyBsBYSy-QRBGY2EtJtDpOSewDr3-PHq-cM",
+  "authDomain": "studio-5718849531-9c652.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "589442988743"
+};
